cec8c1f30dad62e3a9bf98bd5592c581e4400acf
Fan Wu <iamnarutowu@gmail.com>
2015-10-25 15:48:47 -0500

    first try. added comments about 2 tries.

--- Remove Duplicates from Sorted List/1.py
+++ Remove Duplicates from Sorted List/1.py
@@ -1,3 +1,8 @@
+# the first try is better than the second try
+# 1. you only need one variable temp to do the loop;
+# 2. you only need one while sentence. Even though in 2.py, the time complexity
+#   is also O(n)
+
 class Solution:
     # @param head, a ListNode
     # @return a ListNode
