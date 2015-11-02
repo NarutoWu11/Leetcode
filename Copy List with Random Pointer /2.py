ee630ac781086742b303552e1cd59f3f9f720fbe
Fan Wu <iamnarutowu@gmail.com>
2015-11-02 13:23:14 -0600

    second try at 11.2.2015. decrease time-limit to 100+ ms. from 300+ms

---
+++ Copy List with Random Pointer /2.py
@@ -0,0 +1,40 @@
+# Definition for singly-linked list with a random pointer.
+# class RandomListNode:
+#     def __init__(self, x):
+#         self.label = x
+#         self.next = None
+#         self.random = None
+
+class Solution:
+    # @param head, a RandomListNode
+    # @return a RandomListNode
+    def copyRandomList(self, head):
+        if head == None:
+            return None
+        
+        temp = head
+        head2 = RandomListNode(temp.label)
+        mapofNodes = {head: head2}
+        temp2 = head2
+        
+        while temp.next:
+            temp2.next = RandomListNode(temp.next.label)
+            mapofNodes[temp.next] = temp2.next
+            
+            temp = temp.next
+            temp2 = temp2.next
+        
+        temp = head
+        temp2 = head2
+        while temp:
+            if temp.random != None:
+                mapofNodes[temp].random = mapofNodes[temp.random]
+            
+            temp = temp.next
+        
+        return head2
+        
+        
+        
+        
+        
