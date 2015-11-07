b33243c12e7c0028e215b600e9208fed6577a5fb
Fan Wu <iamnarutowu@gmail.com>
2015-11-06 21:49:34 -0600

    second try

---
+++ Add Two Numbers/2.py
@@ -0,0 +1,53 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        if not l1 or not l2:
+            return l1 or l2
+        else:
+            # l1 and l2 are both not None
+            carry = 0
+            start = ListNode(0)
+            temp = start
+            
+            next_carry = 0
+            
+            while l1 or l2:
+                if l1 and l2:
+                    next_carry = (l1.val + l2.val + carry) / 10
+                    l1.val = (l1.val + l2.val + carry) % 10
+                    temp.next = l1
+                    
+                    temp = temp.next
+                    l1 = l1.next
+                    l2 = l2.next
+                    
+                
+                elif not l1:
+                    next_carry = (l2.val + carry) / 10
+                    l2.val = (l2.val + carry) % 10
+                    temp.next = l2
+                    
+                    temp = temp.next
+                    l2 = l2.next
+                elif not l2:
+                    next_carry = (l1.val + carry) / 10
+                    l1.val = (l1.val + carry) % 10
+                    temp.next = l1
+                    
+                    temp = temp.next
+                    l1 = l1.next
+                carry = next_carry
+                
+            temp.next = ListNode(1) if carry else None
+            return start.next
+            
