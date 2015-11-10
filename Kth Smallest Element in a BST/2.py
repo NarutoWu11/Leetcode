31203974d5f5c289069f42e6efc6e33ccf2d7b15
Fan Wu <iamnarutowu@gmail.com>
2015-11-10 16:00:38 -0600

    second try at 11.10.2015 using inorder traversal

---
+++ Kth Smallest Element in a BST/2.py
@@ -0,0 +1,39 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+class Solution(object):
+    # @param {TreeNode} root
+    # @param {integer} k
+    # @return {integer}
+    def __init__(self):
+        self.count = 0
+        self.result = 0
+    
+    def kthSmallest(self, root, k):
+        self.count = k
+        self.findRightOne(root)
+        return self.result
+    
+    def findRightOne(self, root):
+        if root.left:
+            self.findRightOne(root.left)
+        
+        #must make sure that 
+        if self.count == 0:
+            return
+        
+        if self.count - 1 == 0:
+            self.count = 0
+            self.result = root.val
+            return
+        else:
+            self.count -= 1
+            if root.right:
+                self.findRightOne(root.right)
+            else:
+                return
