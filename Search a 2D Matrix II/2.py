2a83040f35b72fa1b6749c7f5d6567104b454fd4
Fan Wu <iamnarutowu@gmail.com>
2015-11-22 17:53:00 -0600

    Second try. starting at the left-down corner of the matrix. O(n) = m + 1

---
+++ Search a 2D Matrix II/2.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        
+        if not matrix:
+            return False
+        else:
+            i, j = len(matrix) - 1, 0
+            while i > -1 and j < len(matrix[0]):
+                if matrix[i][j] == target:
+                    return True
+                elif matrix[i][j] > target:
+                    i -= 1
+                else:
+                    j += 1
+            return False
+        
