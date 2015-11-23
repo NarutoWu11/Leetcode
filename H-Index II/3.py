e911354e406d118e859b3f6ff70a8f4651199316
Fan Wu <iamnarutowu@gmail.com>
2015-11-22 23:40:52 -0600

    second try at 11.22.2015

---
+++ H-Index II/3.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def hIndex(self, citations):
+        """
+        :type citations: List[int]
+        :rtype: int
+        """
+        length = len(citations)
+        if not length:
+            return 0
+        
+        left, right = 0, length-1
+    
+        while left < right:
+            mid = (left+ right) >> 1
+            if length - mid > citations[mid]:
+                left = mid + 1
+            elif length - mid == citations[mid]:
+                return citations[mid]
+            else:
+                right = mid - 1
+        return (length - left, length - left - 1 )[ length - left> citations[left] ]
