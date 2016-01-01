5bdd7e630ddc017135b8d37937581d99eef5f0a6
Fan Wu <iamnarutowu@gmail.com>
2015-12-31 22:03:31 -0600

    use 3 array to dp

---
+++ BestTimetoBuyandSellStockwithCooldown/1_3arraies.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        sell = [0,]
+        buy = [-sys.maxint-1,]
+        cool = [0,]
+        for x in prices:
+            new_sell = max(sell[-1], buy[-1] + x)
+            new_buy = max(buy[-1], cool[-1] - x)
+            new_cool = sell[-1]
+            sell.append(new_sell)
+            buy.append(new_buy)
+            cool.append(new_cool)
+        
+        return max(cool[-1],sell[-1])
